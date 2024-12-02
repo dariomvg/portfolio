064f56efa4a738dc8cd9b3fb077588f78d43cd39
dariomvg <contact.dario.info@gmail.com>
2024-12-02 15:59:22 -0300

    add new project

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -17,7 +17,6 @@ export const projects = [
     description: "Una aplicación de películas. Puedes buscar peliculas y series, ver detalles, traílers, próximas a salir y más",
     link: "https://app-movies-chi.vercel.app/",
     repo: "https://github.com/dariomvg/movies-app",
-    effect:"right",
     aprox: true
   },
   {
@@ -28,7 +27,6 @@ export const projects = [
     description: "Librería de paquetes en NPM con diferentes funciones como fechas, animaciones, componentesUI y efecto de maquina de escribir",
     link: "https://docs-kit-elements.vercel.app",
     repo: "https://github.com/dariomvg/docs-kit-elements",
-    effect:"left",
     aprox: true
   },
   {
@@ -39,7 +37,6 @@ export const projects = [
     description: "Aplicación de notas. puedes usarlo para escribir, organizar, y almacenar notas de forma digital",
     link: "https://notes-kappa-ashy.vercel.app/",
     repo: "https://github.com/dariomvg/notes",
-    effect:"right",
     aprox: true
   },
   {
@@ -50,7 +47,6 @@ export const projects = [
     description: "Blog de tecnología. incluye artículos sobre tecnología, programación y desarrollo web",
     link: "https://blog-bytes-kohl.vercel.app/",
     repo: "https://github.com/dariomvg/blog-bytes",
-    effect:"right",
     aprox: true
   },
   {
@@ -61,7 +57,6 @@ export const projects = [
     description: "Aplicación de gestión de proyectos. puedes crear y administrar proyectos, agregar tareas, seguir el progreso de cada proyecto",
     link: "https://gestionar-proyectos.vercel.app/",
     repo: "https://github.com/dariomvg/gestionar-proyectos",
-    effect:"left",
     aprox: true
   },
     {
@@ -72,7 +67,6 @@ export const projects = [
     description: "Aplicación donde puedes crear editores de texto con múltiples herramientas, desde links hasta diferentes tamaños de texto",
     link: "https://text-lab.vercel.app/",
     repo: "https://github.com/dariomvg/text-lab",
-    effect: "right",
     aprox: true
   },
   {
@@ -83,7 +77,6 @@ export const projects = [
     description: "Repositorio de una app de tareas, dónde puedes crear notas personales más subida de imágenes",
     link: "",
     repo: "https://github.com/dariomvg/tasks-fullstack",
-    effect: "left",
     aprox: true
   },
   {
@@ -94,7 +87,6 @@ export const projects = [
     description: "Repositorio de una app de creación de chats en tiempo real, con invitación privada con contraseñas, rápido y fáciil de usar",
     link: "",
     repo: "https://github.com/dariomvg/chats-in-realtime",
-    effect: "right",
     aprox: true
   },
   {
@@ -105,7 +97,6 @@ export const projects = [
     description: "Gestor Group es una que cuenta con un editor de texto, un todoList, mensajes y en dónde puedes agregar colaboradores",
     link: "https://gestor-group.vercel.app",
     repo: "https://github.com/dariomvg/gestor-group",
-    effect: "left",
     aprox: true
   },
   {
@@ -116,7 +107,6 @@ export const projects = [
     description: "Es una aplicación de comidas donde tienes una lista de comidas y cuenta con otro lista de comidas en ingles",
     link: "https://elige-que-comer.vercel.app",
     repo: "https://github.com/dariomvg/elige-que-comer",
-    effect: "right",
     aprox: true
   },
   {
@@ -127,7 +117,6 @@ export const projects = [
     description: "Es una aplicación de login que cuenta con autenticación, cookies, rutas protegidas y verificaciones",
     link: "",
     repo: "https://github.com/dariomvg/app-auth",
-    effect: "left",
     aprox: true
   },
   {
@@ -138,30 +127,27 @@ export const projects = [
     description: "Puedes ver el paso  a paso de como funcionan los algoritmos complejos, probando cambiando el código y viendo el resultado",
     link: "https://visualizador-algoritmos.vercel.app/",
     repo: "https://github.com/dariomvg/visualizador-algoritmos",
-    effect: "right",
     aprox: true
   },
   {
     id: 13,
-    icons: [iconAstro, iconJs, iconCss, iconType, iconType],
+    icons: [iconAstro, iconJs, iconCss, iconType],
     title: "Que comer",
     date: "Octubre 2024",
     description: "Te elige una comida de latinoamérica aleatoria, cuenta con comidas de peru, brasil, uruguay, argentina",
     link: "https://github.com/dariomvg/que-comer",
     repo: "https://que-comer.vercel.app",
-    effect: "left",
     aprox: true
   },
   {
     id: 14,
     icons: [iconNext, iconJs, iconCss, iconExpress, iconType],
-    title: "Lector Files",
+    title: "Change Images",
     date: "Diciembre",
-    description: "lector files puede convertir de png a jpg, y viceversa. también archivos pdf a docx y viceversa y por último leer archivo EXCEL para mostrar los datos",
-    link: "",
-    repo: "",
-    effect: "left",
-    aprox: false
+    description: "Aplicación para cambiar el formato de tus imagenes y optimizarlas al formato webp",
+    link: "https://change-images.vercel.app",
+    repo: "https://github.com/dariomvg/change-images",
+    aprox: true
   },
   {
     id: 15,
@@ -171,18 +157,16 @@ export const projects = [
     description: "Podes crear documentaciones a traves de plantillas para tus proyectos, documentación markdown profesional y minimalista",
     link: "",
     repo: "",
-    effect: "left",
     aprox: false
   },
   {
     id: 16,
-    icons: [iconNext, iconJs, iconCss, iconType],
+    icons: [iconNext, iconJs, iconExpress, iconType],
     title: "App tickets",
     date: "Enero 2025",
     description: "App venta tickets, para eventos, con autenticación, seguridad y escalable",
     link: "",
     repo: "",
-    effect: "left",
     aprox: false
   },
 ];
