137ef524009a2954f392e551d2c0c48b5a24e268
dariomvg <contact.dario.info@gmail.com>
2025-02-13 19:07:43 -0300

    add three new projects

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -7,7 +7,7 @@ import iconPostgres from "../assets/logos/postgresql.svg";
 import iconExpress from "../assets/logos/express.svg";
 import iconSupabase from "../assets/logos/supabase.svg";
 import iconType from "../assets/logos/typescript.svg";
-import iconNode from "../assets/logos/node.svg"
+import iconNode from "../assets/logos/node.svg";
 import type { ProjectsTypes } from "../types/utils.types";
 
 export const projects: ProjectsTypes[] = [
@@ -16,7 +16,8 @@ export const projects: ProjectsTypes[] = [
     icons: [iconNext, iconJs, iconCss],
     title: "App peliculas",
     date: "Septiembre 2023",
-    description: "Una aplicación de películas. Puedes buscar peliculas y series, ver detalles, traílers, próximas a salir y más",
+    description:
+      "Una aplicación de películas. Puedes buscar peliculas y series, ver detalles, traílers, próximas a salir y más",
     repo: "https://github.com/dariomvg/movies-app",
   },
   {
@@ -24,7 +25,8 @@ export const projects: ProjectsTypes[] = [
     icons: [iconReact, iconJs, iconCss, iconType],
     title: "Kit elements",
     date: "Noviembre 2024",
-    description: "Librería de paquetes en NPM con diferentes funciones como fechas, animaciones, componentesUI y efecto de maquina de escribir",
+    description:
+      "Librería de paquetes en NPM con diferentes funciones como fechas, animaciones, componentesUI y efecto de maquina de escribir",
     repo: "https://github.com/dariomvg/kit-elements",
   },
   {
@@ -32,7 +34,8 @@ export const projects: ProjectsTypes[] = [
     icons: [iconReact, iconJs, iconCss, iconType],
     title: "App de notas",
     date: "Junio 2024",
-    description: "Aplicación de notas. puedes usarlo para escribir, organizar, y almacenar notas de forma digital",
+    description:
+      "Aplicación de notas. puedes usarlo para escribir, organizar, y almacenar notas de forma digital",
     repo: "https://github.com/dariomvg/notes",
   },
   {
@@ -40,7 +43,8 @@ export const projects: ProjectsTypes[] = [
     icons: [iconAstro, iconJs, iconCss],
     title: "Blog bytes",
     date: "Mayo 2024",
-    description: "Blog de tecnología. incluye artículos sobre tecnología, programación y desarrollo web",
+    description:
+      "Blog de tecnología. incluye artículos sobre tecnología, programación y desarrollo web",
     repo: "https://github.com/dariomvg/blog-bytes",
   },
   {
@@ -48,15 +52,17 @@ export const projects: ProjectsTypes[] = [
     icons: [iconNext, iconJs, iconCss, iconType],
     title: "Gestor de proyectos",
     date: "Agosto 2024",
-    description: "Aplicación de gestión de proyectos. puedes crear y administrar proyectos, agregar tareas, seguir el progreso de cada proyecto",
+    description:
+      "Aplicación de gestión de proyectos. puedes crear y administrar proyectos, agregar tareas, seguir el progreso de cada proyecto",
     repo: "https://github.com/dariomvg/gestionar-proyectos",
   },
-    {
+  {
     id: 6,
     icons: [iconNext, iconJs, iconCss, iconType],
     title: "TextLab",
     date: "Septiembre 2024",
-    description: "Aplicación donde puedes crear editores de texto con múltiples herramientas, desde links hasta diferentes tamaños de texto",
+    description:
+      "Aplicación donde puedes crear editores de texto con múltiples herramientas, desde links hasta diferentes tamaños de texto",
     repo: "https://github.com/dariomvg/text-lab",
   },
   {
@@ -64,7 +70,8 @@ export const projects: ProjectsTypes[] = [
     icons: [iconNext, iconJs, iconCss, iconSupabase],
     title: "Tasks fullstack",
     date: "Septiembre 2024",
-    description: "Repositorio de una app de tareas, dónde puedes crear notas personales más subida de imágenes",
+    description:
+      "Repositorio de una app de tareas, dónde puedes crear notas personales más subida de imágenes",
     repo: "https://github.com/dariomvg/tasks-fullstack",
   },
   {
@@ -72,7 +79,8 @@ export const projects: ProjectsTypes[] = [
     icons: [iconReact, iconJs, iconExpress, iconPostgres],
     title: "Chats realtime",
     date: "Agosto 2024",
-    description: "Repositorio de una app de creación de chats en tiempo real, con invitación privada con contraseñas, rápido y fáciil de usar",
+    description:
+      "Repositorio de una app de creación de chats en tiempo real, con invitación privada con contraseñas, rápido y fáciil de usar",
     repo: "https://github.com/dariomvg/chats-in-realtime",
   },
   {
@@ -80,7 +88,8 @@ export const projects: ProjectsTypes[] = [
     icons: [iconNext, iconJs, iconCss, iconSupabase, iconType],
     title: "Gestor Group",
     date: "Septiembre 2024",
-    description: "Gestor Group es una que cuenta con un editor de texto, un todoList, mensajes y en dónde puedes agregar colaboradores",
+    description:
+      "Gestor Group es una que cuenta con un editor de texto, un todoList, mensajes y en dónde puedes agregar colaboradores",
     repo: "https://github.com/dariomvg/gestor-group",
   },
   {
@@ -88,7 +97,8 @@ export const projects: ProjectsTypes[] = [
     icons: [iconNext, iconJs, iconCss],
     title: "Elige que comer",
     date: "Noviembre 2024",
-    description: "Es una aplicación de comidas donde tienes una lista de comidas y cuenta con otro lista de comidas en ingles",
+    description:
+      "Es una aplicación de comidas donde tienes una lista de comidas y cuenta con otro lista de comidas en ingles",
     repo: "https://github.com/dariomvg/elige-que-comer",
   },
   {
@@ -96,7 +106,8 @@ export const projects: ProjectsTypes[] = [
     icons: [iconNext, iconJs, iconCss, iconExpress],
     title: "App login con autenticación",
     date: "Septiembre 2024",
-    description: "Es una aplicación de login que cuenta con autenticación, cookies, rutas protegidas y verificaciones",
+    description:
+      "Es una aplicación de login que cuenta con autenticación, cookies, rutas protegidas y verificaciones",
     repo: "https://github.com/dariomvg/app-auth",
   },
   {
@@ -104,7 +115,8 @@ export const projects: ProjectsTypes[] = [
     icons: [iconNext, iconJs, iconCss],
     title: "Visualizador de algoritmos",
     date: "Octubre 2024",
-    description: "Míra el paso  a paso de como funcionan los algoritmos complejos, probando cambiando el código y viendo el resultado",
+    description:
+      "Míra el paso  a paso de como funcionan los algoritmos complejos, probando cambiando el código y viendo el resultado",
     repo: "https://github.com/dariomvg/visualizador-algoritmos",
   },
   {
@@ -112,7 +124,8 @@ export const projects: ProjectsTypes[] = [
     icons: [iconAstro, iconJs, iconCss, iconType],
     title: "Que comer",
     date: "Octubre 2024",
-    description: "Te elige una comida de latinoamérica aleatoria, cuenta con comidas de peru, brasil, uruguay, argentina",
+    description:
+      "Te elige una comida de latinoamérica aleatoria, cuenta con comidas de peru, brasil, uruguay, argentina",
     repo: "https://github.com/dariomvg/que-comer",
   },
   {
@@ -120,7 +133,8 @@ export const projects: ProjectsTypes[] = [
     icons: [iconNext, iconJs, iconCss, iconExpress, iconType],
     title: "Change Images",
     date: "Diciembre 2024",
-    description: "Aplicación para cambiar el formato de tus imagenes y optimizarlas al formato webp",
+    description:
+      "Aplicación para cambiar el formato de tus imagenes y optimizarlas al formato webp",
     repo: "https://github.com/dariomvg/change-images",
   },
   {
@@ -128,7 +142,8 @@ export const projects: ProjectsTypes[] = [
     icons: [iconNext, iconCss, iconType],
     title: "App calendarUI",
     date: "Diciembre 2024",
-    description: "Puedes organizar tus días, semanas o tu mes completo, agregando tus tareas, proyectos, etc.",
+    description:
+      "Puedes organizar tus días, semanas o tu mes completo, agregando tus tareas, proyectos, etc.",
     repo: "https://github.com/dariomvg/calendarUI",
   },
   {
@@ -136,7 +151,35 @@ export const projects: ProjectsTypes[] = [
     icons: [iconJs, iconNode],
     title: "Docs-generator-readme",
     date: "Enero 2025",
-    description: "Podes crear la documentación para tus proyectos. Usandoló como una base o para un README rápido",
+    description:
+      "Podes crear la documentación para tus proyectos. Usandoló como una base o para un README rápido",
     repo: "https://github.com/dariomvg/docs-generator-readme",
+  },
+  {
+    id: 17,
+    icons: [iconType, iconPostgres, iconNext],
+    title: "Lista de temas",
+    date: "Febrero 2025",
+    description:
+      "Parecido a reddit, puedes agregar un temas, agregar comentarios de opinión, etc.",
+    repo: "https://github.com/dariomvg/app-list-topicse",
+  },
+  {
+    id: 18,
+    icons: [iconNext, iconType, iconCss],
+    title: "Chat IA",
+    date: "Febrero 2025",
+    description:
+      "Chat con IA, para preguntas o conversaciones, añadiendo movilidad al chat",
+    repo: "https://github.com/dariomvg/chat-ia",
+  },
+  {
+    id: 19,
+    icons: [iconType, iconNext, iconCss],
+    title: "App historias",
+    date: "Enero 2025",
+    description:
+      "App que simula la subida de historias, con imagenes, tiempo de carga y registro simple",
+    repo: "https://github.com/dariomvg/app-histories",
   },
 ];
